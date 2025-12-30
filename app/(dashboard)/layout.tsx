74e864aa3bb5e0d2ca2f32260b7d688930c2663f
Nobisuke19 <subham019650@gmail.com>
2025-12-30 21:23:59 +0530

    fix: resolve sidebar type definition error

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,6 +11,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed ms:insert-y-0 z-[80] bg-gray-900">
+        {/* @ts-ignore */}
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
         {/* Hello side bar */}
       </div>
