bcba9bccd3e4b6a53d1522c9b4c812fdc243b1c0
Nobisuke19 <subham019650@gmail.com>
2025-12-30 21:03:39 +0530

    Fix build: suppress TS errors and ignore test files

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,10 +11,12 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed ms:insert-y-0 z-[80] bg-gray-900">
+        {/* @ts-expect-error Server Component */}
         <Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />
         {/* Hello side bar */}
       </div>
       <main className="md:pl-72">
+        {/* @ts-expect-error Server Component */}
         <Navbar />
         {children}
       </main>
